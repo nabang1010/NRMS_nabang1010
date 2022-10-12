2f0f205b6c401415c7c1e05f302eca58352ec5d0
nabang1010 <nguyenanhbang1010@gmail.com>
2022-10-12 10:31:40 +0700

    Update data_preprocess.py & ipynb

--- data_preprocess_nabang1010.py
+++ data_preprocess_nabang1010.py
@@ -8,11 +8,12 @@ import json
 from nltk.tokenize import word_tokenize
 import swifter
 
+# ====================================================================================
 
 with open("config.yaml", "r") as f:
     config = yaml.load(f, Loader=yaml.FullLoader)
 
-# def behaviors_file_process(source, target, user2int_path):
+# ====================================================================================
 
 
 def row_process(row, category2int, entity2int, word2int):
@@ -71,6 +72,9 @@ def row_process(row, category2int, entity2int, word2int):
             "abstract_entities",
         ],
     )
+
+
+# ====================================================================================
 
 
 def behaviors_file_process(source, target, user2int_path):
@@ -138,6 +142,9 @@ def behaviors_file_process(source, target, user2int_path):
     )
 
 
+# ====================================================================================
+
+
 def news_file_process(
     source, target, category2int_path, word2int_path, entity2int_path, mode
 ):
@@ -169,17 +176,114 @@ def news_file_process(
         word2freq = {}
         entity2int = {}
         entity2freq = {}
+        for row in df_news.itertuples(index=False):
 
+            # gộp cả category và subcategory vào category2int
+
+            if row.category not in category2int:
+                category2int[row.category] = len(category2int) + 1
+            if row.subcategory not in category2int:
+                category2int[row.subcategory] = len(category2int) + 1
+
+            # ----- đếm số lần xuất hiện của các từ trong title và abstract
+            for w in word_tokenize(
+                row.title.lower()
+            ):  # tokenize title đã được chuyển thành chữ thường
+                if w not in word2freq:
+                    word2freq[w] = 1
+                else:
+                    word2freq[w] += 1
+
+            for w in word_tokenize(
+                row.abstract.lower()
+            ):  # tokenize abstract đã được chuyển thành chữ thường
+                if w not in word2freq:
+                    word2freq[w] = 1
+                else:
+                    word2freq[w] += 1
+
+            for e in json.loads(row.title_entities):
+                times = len(e["OccurrenceOffsets"]) * e["Confidence"]
+                if times > 0:
+                    if e["WikidataId"] not in entity2freq:
+                        entity2freq[e["WikidataId"]] = times
+                    else:
+                        entity2freq[e["WikidataId"]] += times
+
+            for e in json.loads(row.abstract_entities):
+                times = len(e["OccurrenceOffsets"]) * e["Confidence"]
+                if times > 0:
+                    if e["WikidataId"] not in entity2freq:
+                        entity2freq[e["WikidataId"]] = times
+                    else:
+                        entity2freq[e["WikidataId"]] += times
+
+        for k, v in word2freq.items():
+            if v >= config["BASE_CONFIG"]["word_freq_threshold"]:
+                word2int[k] = len(word2int) + 1
+
+        for k, v in entity2freq.items():
+            if v >= config["BASE_CONFIG"]["entity_freq_threshold"]:
+                entity2int[k] = len(entity2int) + 1
+
+        parsed_news = df_news.swifter.apply(row_process, axis=1)
+
+        pd.DataFrame(category2int.items(), columns=["category", "int"]).to_csv(
+            category2int_path, sep="\t", index=False
+        )
+
+        pd.DataFrame(word2int.items(), columns=["word", "int"]).to_csv(
+            word2int_path, sep="\t", index=False
+        )
+        pd.DataFrame(entity2int.items(), columns=["entity", "int"]).to_csv(
+            entity2int_path, sep="\t", index=False
+        )
+
+    elif mode == "test":
+        category2int = dict(pd.read_table(category2int_path).values.tolist())
+
+        word2int = dict(pd.read_table(word2int_path, na_filter=False).values.tolist())
+
+        entity2int = dict(pd.read_table(entity2int_path).values.tolist())
+
+        parsed_news = df_news.swifter.apply(row_process, axis=1)
+
+        parsed_news.to_csv(target, sep="\t", index=False)
+
+    else:
+        raise ValueError("mode must be train or test")
+
+
+# ====================================================================================
+
+
+# ====================================================================================
 
 if __name__ == "__main__":
     DATA_RAW = "/workspace/nabang1010/LBA_NLP/Recommendation_System/DATA/dev_small/"
     DATA_DIR = "./DATA_nabang1010"
+
     train_dir = "train"
     val_dir = "val"
     test_dir = "test"
 
     source_behaviors = os.path.join(DATA_RAW, "behaviors.tsv")
-    target_behaviors = os.path.join(DATA_DIR, train_dir, "behaviors.tsv")
+    target_behaviors = os.path.join(DATA_DIR, train_dir, "behaviors_preprocessed.tsv")
     user2int_path = os.path.join(DATA_DIR, train_dir, "user2int.tsv")
 
+    source_news = os.path.join(DATA_RAW, "news.tsv")
+    target_news = os.path.join(DATA_DIR, train_dir, "news_preprocessed.tsv")
+    category2int_path = os.path.join(DATA_DIR, train_dir, "category2int.tsv")
+    word2int_path = os.path.join(DATA_DIR, train_dir, "word2int.tsv")
+    entity2int_path = os.path.join(DATA_DIR, train_dir, "entity2int.tsv")
+
     behaviors_file_process(source_behaviors, target_behaviors, user2int_path)
+
+    news_file_process(
+        source_news,
+        target_news,
+        category2int_path,
+        word2int_path,
+        entity2int_path,
+        mode="train",
+    )
