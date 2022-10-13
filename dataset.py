ad3f1e4ed33ea1fa86ae1b8e60948e920f713f1d
nabang1010 <nguyenanhbang1010@gmail.com>
2022-10-13 16:17:26 +0700

    building dataset.py

---
+++ dataset.py
@@ -0,0 +1,87 @@
+import torch 
+from torch.utils.data import Dataset
+import pandas as pd
+from ast import literal_eval
+import yaml
+
+with open("config.yaml", "r") as f:
+    config = yaml.load(f, Loader=yaml.FullLoader)
+
+
+
+class BaseDataset(Dataset):
+    def __init__(self, behaviors_path, news_path):
+        super(BaseDataset, self).__init__()
+        assert all(attribute in [
+            'category', 'subcategory', 'title', 'abstract', 'title_entities',
+            'abstract_entities'
+        ] for attribute in  config["NRMS_CONFIG"]["dataset_attributes"]['news'])
+        assert all(attribute in ['user', 'clicked_news_length']
+                   for attribute in config["NRMS_CONFIG"]["dataset_attributes"]['record'])
+
+        self.behaviors_parsed = pd.read_table(behaviors_path)
+        self.news_parsed = pd.read_table(
+            news_path,
+            index_col='id',
+            usecols=['id'] + config["NRMS_CONFIG"]["dataset_attributes"]['news'],
+            converters={
+                attribute: literal_eval
+                for attribute in set(config["NRMS_CONFIG"]["dataset_attributes"]['news']) & set([
+                    'title', 'abstract', 'title_entities', 'abstract_entities'
+                ])
+            })
+
+
+        self.news_id2int = {x: i for i, x in enumerate(self.news_parsed.index)}
+        self.news2dict = self.news_parsed.to_dict('index')
+        for key1 in self.news2dict.keys():
+            for key2 in self.news2dict[key1].keys():
+                self.news2dict[key1][key2] = torch.tensor(
+                    self.news2dict[key1][key2])
+        padding_all = {
+            'category': 0,
+            'subcategory': 0,
+            'title': [0] * config["BASE_CONFIG"]["num_words_title"],
+            'abstract': [0] * config["BASE_CONFIG"]["num_words_abstract"],
+            'title_entities': [0] * config["BASE_CONFIG"]["num_words_title"],
+            'abstract_entities': [0] * config["BASE_CONFIG"]["num_words_abstract"]
+        }
+        for key in padding_all.keys():
+            padding_all[key] = torch.tensor(padding_all[key])
+
+        self.padding = {
+            k: v
+            for k, v in padding_all.items()
+            if k in config["NRMS_CONFIG"]["dataset_attributes"]['news']
+        }
+
+    def __len__(self):
+        return len(self.behaviors_parsed)
+
+    def __getitem__(self, idx):
+        item = {}
+        row = self.behaviors_parsed.iloc[idx]
+        if 'user' in config["NRMS_CONFIG"]["dataset_attributes"]['record']:
+            item['user'] = row.user
+        item["clicked"] = list(map(int, row.clicked.split()))
+        item["candidate_news"] = [
+            self.news2dict[x] for x in row.candidate_news.split()
+        ]
+        item["clicked_news"] = [
+            self.news2dict[x]
+            for x in row.clicked_news.split()[:config["BASE_CONFIG"]["num_clicked_news_a_user"]]
+        ]
+        if 'clicked_news_length' in config["NRMS_CONFIG"]["dataset_attributes"]['record']:
+            item['clicked_news_length'] = len(item["clicked_news"])
+        repeated_times = config["BASE_CONFIG"]["num_clicked_news_a_user"] - \
+            len(item["clicked_news"])
+        assert repeated_times >= 0
+        item["clicked_news"] = [self.padding
+                                ] * repeated_times + item["clicked_news"]
+
+        return item
+
+
+
+
+
